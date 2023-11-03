6c935cfaa6f2eba52c18d4c79e487a10d0c3b808
Tamás Zoltán <96738810+CymarOnCreel@users.noreply.github.com>
2023-11-03 18:43:27 +0100

    Update created folders
    - prepared the folders
    - moved the existing files to the folders

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,9 +11,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("/application/view/MainWindow.fxml"));
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/application/view/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
