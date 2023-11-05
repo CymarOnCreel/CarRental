65cfb61146658bb971e5a62083babd08941ff2dc
Tamás Zoltán <96738810+CymarOnCreel@users.noreply.github.com>
2023-11-05 06:52:33 +0100

    Update main window
    - added elements to main window
    - added title and icon to main window

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,15 +3,19 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane root = (GridPane) FXMLLoader.load(getClass().getResource("/application/view/MainWindow.fxml"));
-			Scene scene = new Scene(root, 400, 400);
+			primaryStage.setTitle("Cymar Car Rental");
+			Image image=new Image("/application/resources/icon.jpg");
+			primaryStage.getIcons().add(image);
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/application/view/MainWindow.fxml"));
+			Scene scene = new Scene(root, 800, 600);
 			scene.getStylesheets().add(getClass().getResource("/application/view/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
