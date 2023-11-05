cabc5fbfd3691969057cd6a9d7b05c87dfd46175
Tamás Zoltán <96738810+CymarOnCreel@users.noreply.github.com>
2023-11-05 17:42:27 +0100

    Created registretion window
    - window created
    - controller class created
    - added a few To-Do to login and registration controller

--- src/application/controller/LoginWindowController.java
+++ src/application/controller/LoginWindowController.java
@@ -7,11 +7,20 @@ public class LoginWindowController {
 	
 	@FXML
 	private void switchToSignUpPage() {
+		//To-DO switching to RegistrationWindow
 		System.out.println("switch to sign up");
 	}
 	
 	@FXML
 	private void loginUser() {
+		/* To-Do
+		 * check for username and password match in database
+		 * check for user type and change to appropriate window
+		 * 1. user main window
+		 * 2. salesman window
+		 * 3. admin window
+		 */
+		
 		System.out.println("login User");
 	}
 }
---
+++ src/application/controller/RegistrationWindwoController.java
@@ -0,0 +1,18 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+
+public class RegistrationWindwoController {
+
+	/*TO-DO
+	 * check username is available
+	 * check if both password fields are the same
+	 * check if email is right format contains text before @ and text before . and after . also 
+	 * add user to database
+	 */
+	
+	@FXML
+	private void RegisterUser() {
+		System.out.println("register");
+	}
+}
